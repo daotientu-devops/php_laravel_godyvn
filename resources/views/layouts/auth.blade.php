ca345c49740205e92a7c20fd13692cfb8ede8fe8
tudao <nevarda9416@gmail.com>
2023-04-26 13:04:21 +0700

    - header meta name viewport maximum-scale=1

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <title>N2CMS</title>
     <!-- Bootstrap -->
     <link href="{{ asset(env('FOLDER_PUBLIC').'vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
